f107ab4e1c58e396026d766283f83c138bada7d6
John Havenar <106387886+johnhavenar@users.noreply.github.com>
2022-06-17 08:40:15 -0400

    Update run-script-in-subdirectories.sh

--- misc/run-script-in-subdirectories.sh
+++ misc/run-script-in-subdirectories.sh
@@ -4,16 +4,19 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 echo $SCRIPT_DIR
 
+# Script to be run in subdirectories.
+SCRIPT="<script_name>"
+
 # Get list of directories.
 IFS=$'\n'
-file_list="$(find . -type f -name '*0.???.png' -exec dirname "{}" \; |sort -u)"
+file_list="$(find . -type f -name '<pattern>' -exec dirname "{}" \; |sort -u)"
 
 # Go to directory and run script.
 for directory in ${file_list[@]}; do
     cd $directory
     pwd
-    cp "$SCRIPT_DIR/convert-aspect-ratio.sh" .
-    bash "convert-aspect-ratio.sh"
-    rm "convert-aspect-ratio.sh"
+    cp "$SCRIPT_DIR/$SCRIPT" .
+    bash "$SCRIPT"
+    rm "$SCRIPT"
     cd $SCRIPT_DIR
 done
