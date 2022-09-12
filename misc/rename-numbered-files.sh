53fb5f3805d76e1c8f1ef659bdd04dda78a481f6
John Havenar <106387886+johnhavenar@users.noreply.github.com>
2022-09-12 08:07:18 -0400

    Updated rename-numbered-file.sh
    Added functionality for the numbering to be shifted in order to renumber items when renaming.

--- misc/rename-numbered-files.sh
+++ misc/rename-numbered-files.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Renames files in a directory.
+
+# Directory of files.
 file_dir="<directory-of-files>"
+# Name of files in series.
 file_name="<file-name>"
 file_type="png"
+# Phrase which separates the file and the numbering.
 num_field_char="_"
+# Field width of numbering.
 num_field_width=6
+# First number in series.
+num_first=1
+# Shift the numbering by this amount to renumber items.
+num_shift=0
 
-a=1
+a=$(($num_first+$num_shift))
 for i in $file_dir/*.${file_type}; do
   new=$(printf "$file_dir/${file_name}${num_field_char}%0${num_field_width}d.${file_type}" "$a")
   mv -i -- "$i" "$new"
